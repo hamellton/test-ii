62d8b688d04f71adfac4a2db264893992b0b267f
Kostiantyn Postoian <kostiantyn.postoian@lengin.com>
2024-10-19 19:40:27 +0300

    updated contentful api

---
+++ pages/api/contentful/page-content.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@utils/db";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+}
